466836593fe0576b7a90311225635635d289f24e
Jesús F. Vázquez Biltre <jesusfvb2@gmail.com>
2023-07-07 01:36:38 +0200

    update how to show data time

--- lib/mapper/user_mapper.dart
+++ lib/mapper/user_mapper.dart
@@ -3,9 +3,15 @@ import 'package:congresso_terciarios/dto/user_dto.dart';
 class UserMapper {
   static Map<String, UserDto> fromRowListToMap(List<List<String>> rows) {
     Map<String, UserDto> users = {};
+    var epoch = DateTime(1899, 12, 30);
     for (var row in rows) {
       if (row.isNotEmpty) {
-        users[row[3]] = UserDto(row[3], row[0], row[1], row[2], row.length <= 4 ? "" : row[4],
+        var date = "";
+        if (row[1] != null && row[1] != "") {
+          var temp = epoch.add(Duration(days: int.parse(row[1])));
+          date = "${temp.day}/${temp.month}/${temp.year}";
+        }
+        users[row[3]] = UserDto(row[3], row[0], date, row[2], row.length <= 4 ? "" : row[4],
             row.length <= 5 ? "" : row[5]);
       }
     }
